a534d6bf079f811172201c724ec4c695cafaf393
Prantik Saha <sahaprantik883@gmail.com>
2026-06-01 22:02:18 +0530

    latest commit

--- backend/main.py
+++ backend/main.py
@@ -52,10 +52,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-    "http://localhost:5173",
-    "https://phishing-url-detection-system-psi.vercel.app/",
-    ],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
